8da91c2f0b4331685a09896f2918f7b04e2f2977
Freroma-fahumada <fmahumada@gmail.com>
2020-07-07 22:30:56 -0500

    upload module v1

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Freroma, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Freroma_OptionsAutocomplete', __DIR__);
